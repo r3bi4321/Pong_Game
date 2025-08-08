b10e980f7bca35418323bcbe00d0d519c6841be4
r3bi4321 <110892622+r3bi4321@users.noreply.github.com>
2025-08-08 15:49:48 +0200

    08.08.2025 1.2
    Design for the settings done. Logic is missing

---
+++ Pong_Game/Endpage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pong_Game
+{
+    /// <summary>
+    /// Interaction logic for Endpage.xaml
+    /// </summary>
+    public partial class Endpage : Window
+    {
+        public Endpage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenStartWindow(object sender, RoutedEventArgs e)
+        {
+            Startseite startWindow = new();
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ Pong_Game/StoreWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pong_Game
+{
+    /// <summary>
+    /// Interaction logic for StoreWindow.xaml
+    /// </summary>
+    public partial class StoreWindow : Window
+    {
+        public StoreWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenStartWindow(object sender, RoutedEventArgs e)
+        {
+            Startseite startwindow = new();
+            this.Visibility = Visibility.Hidden; 
+            startwindow.Show();
+        }
+    }
+}
